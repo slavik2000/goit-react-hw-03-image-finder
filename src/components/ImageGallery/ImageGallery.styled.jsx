d3e431859e9907a834e0b85976b11beebdb32fa2
sяva <slavik.bieriezhnoi1995@gmail.com>
2024-08-18 18:43:45 +0300

    first commit

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -4,7 +4,7 @@ export const Gallery = styled.ul`
   display: grid;
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  grid-gap: 16px;
+  grid-gap: 15px;
   margin-top: 0;
   margin-bottom: 0;
   padding: 0;
